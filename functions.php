d0b1642a18a45c86fe5ef526d3b1852bc36f0fff
Baiamatheus <112718146+Baiamatheus@users.noreply.github.com>
2025-07-21 10:14:36 -0300

    v.1

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
-function meu_tema_enqueue_styles() {
+function meu_tema_scripts() {
+    // 1. Carrega o style.css da raiz (obrigatório para o WordPress)
     wp_enqueue_style('meu-tema-style', get_stylesheet_uri());
+
+    // 2. Carrega seu CSS real da pasta assets/styles/
+    wp_enqueue_style('meu-css-custom', get_template_directory_uri() . '/assets/css/main.css');
 }
-add_action('wp_enqueue_scripts', 'meu_tema_enqueue_styles');
+add_action('wp_enqueue_scripts', 'meu_tema_scripts');
